1c7b1ec71fa9872f366292cac044151b6256cc6b
LiamPKenna <liampkenna@gmail.com>
2020-01-31 13:43:04 -0800

    Add scope for random Quiz

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -2,4 +2,6 @@ class Quiz < ApplicationRecord
   has_many :questions, dependent: :destroy
   has_many :finishes, dependent: :destroy
   validates :title, :sub_title, presence: true
+
+  scope :random, -> { order(Arel.sql('random()')).first }
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -3,4 +3,10 @@ require 'rails_helper'
 describe Quiz do
   it { should have_many(:questions) }
   it { should have_many(:finishes) }
+
+  it "should return a random quiz" do
+    Quiz.create!({title:"test title", sub_title: "test sub title"})
+    Quiz.create!({title:"test title", sub_title: "test sub title"})
+    expect(Quiz.random['title']).to(eq('test title'))
+  end
 end
