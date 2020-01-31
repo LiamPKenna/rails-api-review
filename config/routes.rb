63b2f4b077c7485d7e0070060355bba4db84ba51
LiamPKenna <liampkenna@gmail.com>
2020-01-31 08:26:54 -0800

    Add scaffold for answers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :quizzes do
     resources :questions do
-      
+      resources :answers
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
