8f6291e23a7c5f756e7643c373718adcf4ce1a80
LiamPKenna <liampkenna@gmail.com>
2020-01-31 11:34:40 -0800

    Add tests for all quizzes, patch, and delete

--- spec/requests/quiz_route_spec.rb
+++ spec/requests/quiz_route_spec.rb
@@ -9,9 +9,37 @@ describe "quiz routes", :type => :request do
     @quiz = Quiz.all.first
   end
 
-  it 'returns the quiz title' do
+  it 'returns a single quiz' do
     get "/quizzes/#{@quiz.id}"
     expect(JSON.parse(response.body)['title']).to eq('test title')
   end
+
+  it 'returns an array of all quizzes' do
+    get "/quizzes"
+    expect(JSON.parse(response.body).length).to eq(1)
+  end
+
+  it 'allows creation of a new quiz' do
+    Quiz.create!({title:"new test title", sub_title: "new test sub title"})
+    get "/quizzes"
+    expect(JSON.parse(response.body).length).to eq(2)
+  end
+
+  it 'allows updating of a quiz' do
+    @quiz2 = Quiz.new({title:"new test title", sub_title: "new test sub title"})
+    @quiz2.save
+    patch "/quizzes/#{@quiz2.id}", params: { :quiz => {:title => 'updated'} }
+    get "/quizzes/#{@quiz2.id}"
+    expect(JSON.parse(response.body)['title']).to eq('updated')
+  end
+
+  it 'allows deleting of a quiz' do
+    @quiz2 = Quiz.new({title:"new test title", sub_title: "new test sub title"})
+    @quiz2.save
+    delete "/quizzes/#{@quiz2.id}"
+    get "/quizzes"
+    expect(JSON.parse(response.body).length).to eq(1)
+  end
+
 
 end
